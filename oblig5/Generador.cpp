7265d4392e607dc68b71ab08f0b2deaa620171db
Andreas Fast <andis.machine@gmail.com>
2012-05-27 20:29:07 -0300

    Primer version (y ultima?)

---
+++ oblig5/Generador.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#define DEBUG 0
+
+using namespace std;
+
+class Generador {
+  ifstream archivo;
+  istringstream lineaActual;
+  char * nombreArchivo;
+  bool descartarPrimero;
+  int a, ancho; //extremos del intervalo
+  
+  public:
+    void iniciar(char * nombre, bool descartarPrimerNumero) {
+      nombreArchivo = nombre;
+      descartarPrimero = descartarPrimerNumero;
+      a = 0;
+      ancho = 1;
+      archivo.open(nombreArchivo);
+      readLineFromFile();
+    }
+    void iniciar(char * nombre, bool descartarPrimerNumero, int extremo_a, int extremo_b) {
+      nombreArchivo = nombre;
+      descartarPrimero = descartarPrimerNumero;
+      a = extremo_a;
+      ancho = extremo_b - a;
+      archivo.open(nombreArchivo);
+      readLineFromFile();
+    }
+    void readLineFromFile() {
+      string line;
+      int descartado;
+      if (!archivo.is_open())
+        archivo.open(nombreArchivo);
+      if (archivo.eof()) {
+        if (DEBUG)
+        cout << "End of file detected, start anew" << endl;
+        archivo.clear();
+        archivo.seekg(0);
+        if (DEBUG)
+        cout << "File state: " << archivo.good() << endl;
+      }
+      if (archivo.good()) {
+        getline(archivo, line);
+        if (DEBUG)
+        cout << "Linea leida: " << line << endl;
+        lineaActual.str(line);
+        lineaActual.clear();
+        lineaActual.seekg(0);
+        if (descartarPrimero) {
+          lineaActual >> descartado;
+          if (DEBUG)
+          cout << "Descartado: " << descartado << endl;
+        }
+      }
+    }
+    double getNextRand() {
+      return (double)(getNextNum()-a)/ancho;
+    }
+    int getNextNum() {
+      int result;
+      if (DEBUG)
+      cout << "Linea Actual: " << lineaActual.eof() << endl;
+      if (lineaActual.eof())
+        readLineFromFile();
+      lineaActual >> result;
+      return result;
+    }
+      
+    bool lecturaCompleta() {
+      return lineaActual.eof() && archivo.eof();
+    }
+    void cerrar() {
+      archivo.close();
+    }
+    void normalizar() {
+      int max, min, aux;
+
+      cout << "Normalizando el generador para generar entre [0,1]... ";
+      max = min = getNextNum();
+      
+      while (!lecturaCompleta()) {
+        aux = getNextNum();
+        if (aux > max)
+          max = aux;
+        if (aux < min)
+          min = aux;
+      }
+      a = min;
+      ancho = max-min;
+      cout << "normalizado" << endl;
+      archivo.clear();
+      archivo.seekg(0);
+    }
+};
---
+++ oblig5/ej41.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <time.h>
+#include <sys/time.h>
+#include <cmath>
+#include "Generador.cpp"
+
+double segundos_transcurridos(struct timeval *a, struct timeval *b)
+{
+  return
+    (double)(a->tv_sec + (double)a->tv_usec/1000000) -
+    (double)(b->tv_sec + (double)b->tv_usec/1000000);
+}
+
+int puntoPertenece(double x1,double x2,double x3,double x4){
+    if((5*x1 + 10*x2 <= 4) && (x3 + x4 <= 1) && (2*x1 - 3*x2 >= 0) && // restricciones extra
+        (sqrt(pow(x1-0.3,2) + pow(x2-0.2,2) + pow(x3-0.7,2) + pow(x4-0.8,2)) < 0.5)){ // pertenencia al circulo 
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 4) {
+    cout << "Invocar con <archivo> <numero de iteraciones> <ignorar primer elem {0,1}>" << endl;
+    return 0;
+  }
+  Generador generador;
+
+  char * nombreArchivo = argv[1];
+  istringstream args(argv[2]);
+  int n, ignorar;
+  struct timeval start, end;
+
+  args >> n; // Configurar iteraciones
+  args.str(argv[3]);
+  args.clear();
+  args.seekg(0);
+  args >> ignorar; // Configurar el ignorar primer numero de la linea o no
+  
+  cout << "Archivo usado: " << nombreArchivo << endl;
+  cout << "Iteraciones: " << n << endl;
+
+  generador.iniciar(nombreArchivo, ignorar==1);
+  generador.normalizar();
+
+
+  gettimeofday(&start, NULL);;
+  
+  double X = 0;
+  double V = 0;
+
+  for(int i=0;i<n;i++){
+      double x1 = generador.getNextRand();
+      double x2 = generador.getNextRand();
+      double x3 = generador.getNextRand();
+      double x4 = generador.getNextRand();
+
+      if(puntoPertenece(x1,x2,x3,x4)){
+          X += 1;
+      }
+
+  }
+
+  generador.cerrar();
+  X=X/n;
+
+  V = X*(1-X)/(n-1);
+
+  gettimeofday(&end, NULL);
+
+  cout << "Volumen = " << X << "\n";
+  cout << "Varianza = " << V << "\n";
+  cout << "Desviación Estándar = " << sqrt(V) << "\n";
+
+  double secs = segundos_transcurridos(&end, &start); //Calcular segundos
+
+  //Calculo del tiempo insumido
+  cout << "Tiempo= " << secs << " segundos\n";
+
+  return 0;
+}
